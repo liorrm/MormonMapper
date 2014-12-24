6e55c958e8d1e2c6f90e006b72885269c285f485
Liorr Yehuda Morrison <liorr.morrison@gmail.com>
2014-12-23 22:02:34 -0800

    adds JavaScript file to reformat temple data into geoJSON

---
+++ public/js/formatJSON.js
@@ -0,0 +1,25 @@
+////////////////////////////////////////////////////////////////////////////////
+//////////////////// SET UP SKELETON FORMAT FOR geoJSON ARRAY //////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+geoJSON = [
+  [
+    {
+      "type": "FeatureCollection",
+      "features": []
+    }
+  ]
+          ]
+
+
+example = {
+            address: "Okpu-Umuoba Road • Off Aba-Owerri Road at Union Bank • Aba, Abia State • Nigeria • ",
+            created_at: "2014-12-23T21:23:03.470Z",
+            id: 1,
+            latitude: "7.356719",
+            link: "http://www.ldschurchtemples.com/aba/",
+            longitude: "5.147644",
+            name: "Aba Nigeria Temple",
+            snippet: "121st operating temple",
+            updated_at: "2014-12-23T21:23:03.470Z"
+          }
