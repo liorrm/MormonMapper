c156568a42343aac0ffd43530e5508b5f774818e
Liorr Yehuda Morrison <liorr.morrison@gmail.com>
2014-12-27 17:59:03 -0800

    adds commented out script used for cleansing and processing external data

--- db/parse.rb
+++ db/parse.rb
@@ -3,48 +3,58 @@ require 'open-uri'
 require 'awesome_print'
 require 'json'
 
-# doc = Nokogiri::HTML(open("http://www.ldschurchtemples.com/temples/"))
+doc = Nokogiri::HTML(open("http://www.ldschurchtemples.com/temples/"))
 
-# ap links = doc.css("table a")
-# links = links[1..-1]
-# co = 1
-# links.each do |l|
-#   ap co
-#   ap l
-#   co = co + 1
-# end
+ap links = doc.css("table a")
+links = links[1..-1]
+co = 1
+links.each do |l|
+  ap co
+  ap l
+  co = co + 1
+end
 
-# # ap links[0]['href'][3..-1]
+# ap links[0]['href'][3..-1]
 
-# temple_pages = []
+temple_pages = []
 
 # links.each do |link|
 #   temple_pages << Nokogiri::HTML(open("http://www.ldschurchtemples.com/#{link['href'][3..-1]}"))
 # end
 
+# ap temple_pages[0]
+
 # File.open('temple_dates.json', "w") {|file| file.write(temple_pages.to_json) }
-# temple_dates = []
 
-# temple_pages.each do |temple|
-#   temple.css('milestone')
-temple_pages = JSON.parse(File.read("db/temple_dates.json"))
+temple_dates = []
 
+temple_pages = JSON.parse(File.read("temple_dates.json"))
 
+# ap temple_pages
+
+name_pattern = /<h1>(.+)<\/h1>/
 milestone_pattern = /milestone">(.+?)<br>/
 
 temple_data = []
-counter = 1
+
 temple_pages.each do |temple|
-  temple_data << temple.scan(milestone_pattern)
+  temple_hash = {}
+  temple_hash[temple.scan(name_pattern).join("")] = temple.scan(milestone_pattern)
+  temple_data << temple_hash
 end
 
+ap temple_data
+
+
 temple_data.each do |temple|
-  temple.each do |milestone|
-    milestone.each do |wtf|
-      wtf.gsub!("</span>", "")
-    end
-  end
-end
+  Temple.where(name: temple)
+# temple_data.map! do |temple_hash|
+#   temple_hash.flatten!
+# end
+#   temple_hash.map do |attribute_array|
+#     attribute_array = attribute_array.flatten!
+#   end
+# end
 
 # ap temple_data
 
