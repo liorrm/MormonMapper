04ed812ddddd005851c6e261b8429bcf02edb31b
Liorr Yehuda Morrison <liorr.morrison@gmail.com>
2014-12-27 19:24:25 -0800

    deletes several scripts from seedfile, restoring its true purpose to seed the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,42 +8,4 @@ temple_data.each do |temple|
   Temple.create(temple)
 end
 
-# temple_pages = JSON.parse(File.read("db/temple_dates.json"))
 
-
-# milestone_pattern = /milestone">(.+?)<br>/
-
-# temple_data = []
-# counter = 1
-# temple_pages.each do |temple|
-#   temple_data << temple.scan(milestone_pattern)
-# end
-
-# temple_data.each do |temple|
-#   temple.each do |milestone|
-#     milestone.each do |wtf|
-#       wtf.gsub!("</span>", "")
-#     end
-#   end
-# end
-
-
-
-#### Script for creating a json copy of database ####
-# File.open('database.json', 'w') do |file|
-#   file.puts Temple.all.order(id: :asc).to_json
-# end
-#######################################################
-
-# ap temple_data
-
-
-# Temple.all.each_with_index do |temple, index|
-#   temple.announcement = temple_data[index][0][0]
-#   temple.save
-# end
-
-
-# Temple.all.each do |temple|
-#   puts "#{temple.id}. #{temple.name}. Announcement: #{temple.announcement}"
-# end
