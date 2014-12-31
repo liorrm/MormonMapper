673a00d5cb41e3e99c8350920ec8816e741ddfde
Liorr Yehuda Morrison <liorr.morrison@gmail.com>
2014-12-31 02:38:21 -0800

    fixes truncated Flickr photo title bug

--- public/js/flickr.js
+++ public/js/flickr.js
@@ -7,24 +7,27 @@ var loadFlickrPhotos = function(templeItem) {
   }
 
   var searchTerm = templeName.join("") + "LDS"
+  console.log(searchTerm)
 
   var apiKey = "ae27bc9d3af17e89122b67b7aa6a86cb"
 
   var searchURL = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=' + apiKey + '&text=' + searchTerm + '&format=json&nojsoncallback=1'
+
+  console.log(searchURL)
 
   $.ajax({
     type: "GET",
     url: searchURL,
     dataType: "json",
     success: function(serverResponse){
+      console.log(serverResponse)
 
       $.each(serverResponse.photos.photo, function(i, item) {
-
+        console.log(item.title)
         var photoURL = 'https://farm' + item.farm + '.staticflickr.com/' + item.server + '/' + item.id + '_' + item.secret + '_b.jpg'
 
-        $(templeItem).append('<a class="photo" href="' + photoURL + '" data-lightbox="' + templeItem.firstChild.innerHTML + '" data-title=' + item.title + '>' + item.title + '</a>')
+        $(templeItem).append('<a class="photo" href="' + photoURL + '" data-lightbox="' + templeItem.firstChild.innerHTML + '" data-title="' + item.title + '">' + item.title + '</a>')
       })
-
     }
   })
 }
